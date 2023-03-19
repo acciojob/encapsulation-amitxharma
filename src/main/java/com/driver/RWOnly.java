2c576e5a6c97945cc6f344a8cb47bd0df0980582
amit sharma <amitsharma75700@gmail.com>
2023-03-19 21:14:26 +0530

    Encapsulation assignment Done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class RWOnly {
+//    Task 2:
+//    create a class named RWOnly in RWOnly.java, with a private member variable name
+    private String name;
+
+    public RWOnly(String name) {
+        this.name=name;
+    }
+
+    //create an constructor
+    
+
+    //getter function
+    public String getName()
+    {
+        return name;
+    }
+    //setter function
+    public void setName(String name)
+    {
+        this.name=name;
+    }
     
 }
